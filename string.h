799936126f19433e4d343d880dfbb2bd5e135663
Ifteda Ahmed-Syed <33703032+ifteda@users.noreply.github.com>
2020-01-29 18:24:16 -0500

    Remove String implementation. Reduce to method stubs

--- string.h
+++ string.h
@@ -10,72 +10,39 @@
  */
 class String : public Object {
 public:
-    size_t size_; // the number of characters
-    char* val_; // the characters
 
     /**
      * Default Constructor
      */
-    String() {
-        size_ = 0;
-        val_ = new char[size_];
-    }
+    String();
 
     /**
      * Constructor
      */
-    String(char* c) {
-        size_ = strlen(c);
-        val_ = new char[size_];
-        for (size_t i = 0; i < size_; i++) {
-            val_[i] = c[i];
-        }
-    }
+    String(char* c);
 
     /**
      * Destructor
      */
-    ~String() {
-        delete[] val_;
-    }
+    ~String();
 
     /** 
      * Compares the given Object with this one for equality
      * Overridden from Object
      * @arg obj Object against which to compare
      */
-    bool equals(Object* obj) {
-        if (obj == nullptr) {
-            return false;
-        }
-
-        String* s = dynamic_cast<String*>(obj);
-
-        if (s == nullptr) {
-            return false;
-        }
-
-        return strcmp(val_, s->val_) == 0;
-    }
+    bool equals(Object* obj);
 
     /**
      * Returns the hash code value for this String
      * Overridden from Object
      */
-    size_t hash() {
-        size_t h = 0;
-        for (int i = 0; i < size_; i++) {
-            h += (int) val_[i];
-        }
-        return h;
-    }
+    size_t hash();
 
     /**
      * Returns the number of characters in this String
      */
-    size_t size() {
-        return size_;
-    }
+    size_t size();
 
     /**
      * Compares the given String to this String
@@ -84,33 +51,19 @@ public:
      * or >0 if it has a higher ASCII value in this String than in the given String
      * @arg str String against which to compare
      */
-    size_t compare(String* str) {
-        return strcmp(val_, str->val_);
-    }
+    size_t compare(String* str);
 
     /** 
      * Concatenates the given String at the end of this String
      * Returns the result of the concatenation
      * @arg str String to concatenate
      */
-    String* concat(String* str) {
-        size_t newSize = size_ + str->size();
-        char* newString = new char[size_];
-        for (int i = 0; i < size_; i++) {
-            newString[i] = val_[i];
-        }
-        for (int i = size_; i < newSize; i++) {
-            newString[i] = str->val_[i - size_];
-        }
-        size_ = newSize;
-        val_ = newString;
-    }
+    String* concat(String* str);
 
     /**
      * Returns this String as a char*
      * Inherited from Object
      */
-    char* to_string() {
-        return val_;
-    }
+    char* to_string();
+    
 };
